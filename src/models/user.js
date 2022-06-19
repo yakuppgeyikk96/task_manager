a08e12863ead3f0d6060d5e17e5fa8ee9628a9ce
Yakup Geyik <yakuppgeyik@gmail.com>
2022-06-19 14:17:01 +0300

    logging added

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
-const { hash } = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -13,6 +14,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
     lowercase: true,
+    unique: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('Email is invalid!')
@@ -25,7 +27,6 @@ const userSchema = new mongoose.Schema({
     trim: true,
     minLength: 7,
     validate(value) {
-      console.log(value)
       if (value.toLowerCase().includes('password')) {
         throw new Error('Password cannot contain "password"')
       }
@@ -40,16 +41,50 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 })
 
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'lx0ex7ox5')
+
+  user.tokens = user.tokens.concat({ token })
+
+  await user.save()
+
+  return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) throw new Error('Unable to login')
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) throw new Error('Unable to login')
+
+  return user
+}
+
+/* Hash the password before saving */
 userSchema.pre('save', async function (next) {
   const user = this
 
   if (user.isModified('password')) {
-    user.password = await hash(user.password, 8)
+    user.password = await bcrypt.hash(user.password, 8)
   }
 
   next()
 })
 
-module.exports = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
